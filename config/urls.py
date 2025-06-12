d3b92caee870916ab9f47c824be3d2af4d06a22c
Alexey <bystrovalexey15@mail.ru>
2025-06-13 01:12:46 +0700

    viewset generics

--- config/urls.py
+++ config/urls.py
@@ -4,9 +4,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", include("users.urls", namespace="users")),
-    ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+                  path("admin/", admin.site.urls),
+                  path("", include("materials.urls", namespace="materials")),
+                  path("", include("users.urls", namespace="users")),
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

744d4e9abf1f147a3c261958a2109b87b2039537
Alexey <bystrovalexey15@mail.ru>
2025-11-07 01:08:57 +0700

    celery

--- config/settings.py
+++ config/settings.py
@@ -170,6 +170,9 @@ LOGGING = {
 
 CELERY_BROKER_URL = "redis://127.0.0.1:6379/0"
 CELERY_RESULT_BACKEND = "redis://127.0.0.1:6379/0"
+CELERY_TIMEZONE = "Australia/Tasmania"
+CELERY_TASK_TRACK_STARTED = True
+CELERY_TASK_TIME_LIMIT = 30 * 60
 
 REST_FRAMEWORK = {
     "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"],
